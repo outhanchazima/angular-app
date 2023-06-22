c8932c46a965cb8596a4d0c28826c5fafebf680e
Outhan Chazima <outhan254@gmail.com>
2023-06-22 17:36:21 +0300

    signup page

--- src/app/modules/authentication/authentication-routing.module.ts
+++ src/app/modules/authentication/authentication-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AuthenticationComponent } from './authentication.component';
 import { SignInComponent } from './pages/sign-in/sign-in.component';
+import { SignUpComponent } from './pages/sign-up/sign-up.component';
 
 const routes: Routes = [
   {
@@ -10,6 +11,7 @@ const routes: Routes = [
     children: [
       { path: '', redirectTo: 'sign-in', pathMatch: 'full' },
       { path: 'sign-in', component: SignInComponent, data: { returnUrl: window.location.pathname } },
+      { path: 'sign-up', component: SignUpComponent },
     ],
   },
 ];
--- src/app/modules/authentication/authentication.module.ts
+++ src/app/modules/authentication/authentication.module.ts
@@ -5,6 +5,7 @@ import { AngularSvgIconModule } from 'angular-svg-icon';
 import { AuthenticationRoutingModule } from './authentication-routing.module';
 import { AuthenticationComponent } from './authentication.component';
 import { SignInComponent } from './pages/sign-in/sign-in.component';
+import { SignUpComponent } from './pages/sign-up/sign-up.component';
 
 
 
@@ -12,6 +13,7 @@ import { SignInComponent } from './pages/sign-in/sign-in.component';
   declarations: [
     AuthenticationComponent,
     SignInComponent,
+    SignUpComponent,
   ],
   imports: [
     CommonModule,
