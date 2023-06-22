e64ff91938bb95bb9d2a090d26bd854560ae4458
Outhan Chazima <outhan254@gmail.com>
2023-06-22 17:49:55 +0300

    sidebar component init

--- src/app/modules/layout/layout.module.ts
+++ src/app/modules/layout/layout.module.ts
@@ -4,12 +4,14 @@ import { AngularSvgIconModule } from 'angular-svg-icon';
 import { SharedModule } from 'src/app/shared/shared.module';
 import { LayoutRoutingModule } from './layout-routing.module';
 import { LayoutComponent } from './layout.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
 
 
 
 @NgModule({
   declarations: [
-    LayoutComponent
+    LayoutComponent,
+    SidebarComponent
   ],
   imports: [CommonModule, LayoutRoutingModule, AngularSvgIconModule.forRoot(), SharedModule]
 })
