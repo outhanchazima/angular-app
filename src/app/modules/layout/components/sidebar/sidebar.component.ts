950c703c535843f7deb7011700d002eea8fc27c4
Outhan Chazima <outhan254@gmail.com>
2023-06-23 13:38:58 +0300

    sidebar component

--- src/app/modules/layout/components/sidebar/sidebar.component.ts
+++ src/app/modules/layout/components/sidebar/sidebar.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { MenuItem } from '../../models/menu.model';
+import { MenuService } from '../../services/menu.service';
 
 @Component({
   selector: 'app-sidebar',
   templateUrl: './sidebar.component.html',
   styleUrls: ['./sidebar.component.scss']
 })
-export class SidebarComponent {
+export class SidebarComponent implements OnInit {
+  public showSideBar$: Observable<boolean> = new Observable<boolean>();
+  public pagesMenu$: Observable<MenuItem[]> = new Observable<MenuItem[]>();
 
+  constructor(private menuService: MenuService) {
+    this.showSideBar$ = this.menuService.showSideBar$;
+    this.pagesMenu$ = this.menuService.pagesMenu$;
+  }
+
+  ngOnInit(): void {}
+
+  public toggleSidebar() {
+    this.menuService.toggleSidebar();
+  }
 }
