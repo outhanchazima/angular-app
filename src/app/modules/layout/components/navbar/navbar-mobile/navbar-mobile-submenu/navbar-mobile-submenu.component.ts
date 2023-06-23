59a5895a885b4fd852193e86bd71b45f5c70a196
Outhan Chazima <outhan254@gmail.com>
2023-06-23 13:37:34 +0300

    navbar component

---
+++ src/app/modules/layout/components/navbar/navbar-mobile/navbar-mobile-submenu/navbar-mobile-submenu.component.ts
@@ -0,0 +1,32 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { SubMenuItem } from 'src/app/modules/layout/models/menu.model';
+import { MenuService } from 'src/app/modules/layout/services/menu.service';
+
+
+@Component({
+  selector: 'app-navbar-mobile-submenu',
+  templateUrl: './navbar-mobile-submenu.component.html',
+  styleUrls: ['./navbar-mobile-submenu.component.scss'],
+})
+export class NavbarMobileSubmenuComponent implements OnInit {
+  @Input() public submenu = <SubMenuItem>{};
+
+  constructor(private menuService: MenuService) {}
+
+  ngOnInit(): void {}
+
+  public toggleMenu(menu: any) {
+    this.menuService.toggleSubMenu(menu);
+  }
+
+  private collapse(items: Array<any>) {
+    items.forEach((item) => {
+      item.expanded = false;
+      if (item.children) this.collapse(item.children);
+    });
+  }
+
+  public closeMobileMenu() {
+    this.menuService.showMobileMenu = false;
+  }
+}
---
+++ src/app/modules/layout/components/navbar/navbar-mobile/navbar-mobilecomponent.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { MenuService } from '../../../services/menu.service';
+
+@Component({
+  selector: 'app-navbar-mobile',
+  templateUrl: './navbar-mobile.component.html',
+  styleUrls: ['./navbar-mobile.component.scss'],
+})
+export class NavbarMobileComponent implements OnInit {
+  public showMobileMenu$: Observable<boolean> = new Observable<boolean>();
+
+  constructor(private menuService: MenuService) {
+    this.showMobileMenu$ = this.menuService.showMobileMenu$;
+  }
+
+  ngOnInit(): void {}
+
+  public toggleMobileMenu(): void {
+    this.menuService.showMobileMenu = false;
+  }
+}
